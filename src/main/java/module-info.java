12d14c18eead2edad10f618cd9b7b26aba2febd6
Kesha Ceniza <kceniza24@gmail.com>
2024-11-25 16:02:38 +0800

    Revert "incorrect files removed"
    This reverts commit b338ebf17dfe4b138268e544d931c1852f799645.
    
    with '#' will be ignored, and an empty message aborts the commit.  # #
    On branch main # Your branch is up to date with 'origin/main'.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.application.movieticketmanagementsystemapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.application.movieticketmanagementsystemapp to javafx.fxml;
-    exports com.application.movieticketmanagementsystemapp;
-}
