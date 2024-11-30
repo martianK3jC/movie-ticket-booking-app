35fcba97c56a23c41f4310b3239fb8fca43c3e23
Kesha Ceniza <kceniza24@gmail.com>
2024-11-30 15:47:27 +0800

    fixed some errors

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module com.application.loginapp {
     requires javafx.controls;
     requires javafx.fxml;
@@ -7,13 +6,5 @@ module com.application.loginapp {
 
     opens com.application.loginapp to javafx.fxml;
     exports com.application.loginapp;
-=======
-module com.application.movieticketbookingmanagementapp {
-    requires javafx.controls;
-    requires javafx.fxml;
 
-
-    opens com.application.movieticketbookingmanagementapp to javafx.fxml;
-    exports com.application.movieticketbookingmanagementapp;
->>>>>>> bc566fe25301ebfadb10ad5f315f3ff0f6a9d011
 }
